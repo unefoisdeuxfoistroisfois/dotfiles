f7eff03de98b3934644f5190c46c2a4ae9710471
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-09-19 17:24:40 +0200

    Modification d'un shaders

--- .config/ghostty/shaders/cursor_smear_fade.glsl
+++ .config/ghostty/shaders/cursor_smear_fade.glsl
@@ -60,8 +60,8 @@ float ease(float x) {
     return pow(1.0 - x, 3.0);
 }
 
-const vec4 TRAIL_COLOR = vec4(1., 1., 1., 1.0);
-const float DURATION = 0.1; //IN SECONDS
+const vec4 TRAIL_COLOR = vec4(1., 1., 0., 1.0);
+const float DURATION = 0.03; //IN SECONDS
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
