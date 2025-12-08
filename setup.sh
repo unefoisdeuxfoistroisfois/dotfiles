bcff20454b12dd4855809e4d48e80bc4720a049b
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-12-08 22:18:42 +0100

    Début d'un executable pour automatisé mon dotfiles

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo -e "${GREEN}CRÉATION DE LIEN SYMBO${NC}"
+
+rm -rf ~/.vim ~/.config
+
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitattributes ~/.gitattributes
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.config ~/.config
+
+# --- AVEC UN FOR ---
+#for file in .gitconfig .gitignore .gitattributes .vimrc .vim; do
+#    ln -sf ~/dotfiles/$file ~/$file
+#done
+
+echo -e "${BLUE}INSTALLATION TERMINÉE${NC}"
