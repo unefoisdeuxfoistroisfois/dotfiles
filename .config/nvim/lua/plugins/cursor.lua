72eba44753a2763de6a6498434875802f4e541a7
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-09 17:50:17 +0100

    Ajout d'un shaders curser pour nvim

---
+++ .config/nvim/lua/plugins/cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_buffers = false,
+    stiffness = 0.6,
+    trailing_stiffness = 0.5,
+    smear_insert_mode = true,
+    cursor_color = "#ffffff",
+  },
+}
