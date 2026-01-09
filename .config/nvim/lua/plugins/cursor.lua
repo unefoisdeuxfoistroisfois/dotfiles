7b82c7021eadde983a2c96a41bfc6ee2d5e38099
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-09 18:21:19 +0100

    Adaptaion de mon envie pour le shaders

--- .config/nvim/lua/plugins/cursor.lua
+++ .config/nvim/lua/plugins/cursor.lua
@@ -1,10 +1,9 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    smear_between_buffers = false,
     stiffness = 0.6,
     trailing_stiffness = 0.5,
-    smear_insert_mode = true,
+    distance_stop_animating = 0.5,
     cursor_color = "#ffffff",
   },
 }
