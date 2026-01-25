6d7c55553e88694c3bf145954cd722ec5c2e5e9e
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-25 18:21:19 +0100

    Ajout de keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Désactiver les flèches en mode Normal
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+
+-- Désactiver les flèches en mode Insert
+vim.keymap.set("i", "<Up>", "<Nop>")
+vim.keymap.set("i", "<Down>", "<Nop>")
+vim.keymap.set("i", "<Left>", "<Nop>")
+vim.keymap.set("i", "<Right>", "<Nop>")
+
+-- Désactiver les flèches en mode Visual
+vim.keymap.set("v", "<Up>", "<Nop>")
+vim.keymap.set("v", "<Down>", "<Nop>")
+vim.keymap.set("v", "<Left>", "<Nop>")
+vim.keymap.set("v", "<Right>", "<Nop>")
