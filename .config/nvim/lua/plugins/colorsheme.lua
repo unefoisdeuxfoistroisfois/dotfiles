32903c0a322395eaf6646d350824d4628ce967bb
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-30 15:49:51 +0100

    Colorsheme.lua pour un colorscheme permant

---
+++ .config/nvim/lua/plugins/colorsheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      --colorscheme = "tokyonight-day",
+      --colorscheme = "tokyonight-moon",
+      --colorscheme = "tokyonight-night",
+      --colorscheme = "tokyonighti-strom",
+    },
+  },
+}
