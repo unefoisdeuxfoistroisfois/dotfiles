6afecf7ce0615ff956f1e3b94378120df6825e53
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-30 15:50:54 +0100

    Suppression de Shades of purple et ajour de commentaires

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Termguicolors pour les bonnes couleurs (dépant du terminal)
 vim.opt.termguicolors = true
-vim.cmd("colorscheme shades_of_purple")
