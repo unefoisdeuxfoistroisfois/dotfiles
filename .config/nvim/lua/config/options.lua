685966292e1d6cf55ba9c193107be3395e0dd61d
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-30 15:52:16 +0100

    Ajout de mes logs 42 pour les header

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- 42 Header
+vim.g.user42 = 'britela-'
+vim.g.mail42 = 'britela-@student.42belgium.be'
+
 -- Désactivation de l'autoformating
 vim.g.autoformat = false
