8d922bd07438f6417344b2b13f91a5c45008b366
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-30 12:23:30 +0100

    Ajout de commentaire

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,20 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
---Désactiver les flèches
--- mode Normal
+-- === DESACTIVATION DES FLECHES ===
+-- Normal
 vim.keymap.set("n", "<Up>", "<Nop>")
 vim.keymap.set("n", "<Down>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 vim.keymap.set("n", "<Right>", "<Nop>")
 
--- mode Insert
+-- Insert
 vim.keymap.set("i", "<Up>", "<Nop>")
 vim.keymap.set("i", "<Down>", "<Nop>")
 vim.keymap.set("i", "<Left>", "<Nop>")
 vim.keymap.set("i", "<Right>", "<Nop>")
 
--- mode Visual
+-- Visual
 vim.keymap.set("v", "<Up>", "<Nop>")
 vim.keymap.set("v", "<Down>", "<Nop>")
 vim.keymap.set("v", "<Left>", "<Nop>")
