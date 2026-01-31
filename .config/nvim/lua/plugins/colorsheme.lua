a67bcefa40da30a17e93a7fc11554971d434e2da
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-01-31 23:38:11 +0100

    Nouveau colorscheme Fluormachine

--- .config/nvim/lua/plugins/colorsheme.lua
+++ .config/nvim/lua/plugins/colorsheme.lua
@@ -1,12 +1,31 @@
+--return {
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "tokyonight",
+--      --colorscheme = "tokyonight-day",
+--      --colorscheme = "tokyonight-moon",
+--      --colorscheme = "tokyonight-night",
+--      --colorscheme = "tokyonighti-strom",
+--    },
+--  },
+--}
+--
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-      --colorscheme = "tokyonight-day",
-      --colorscheme = "tokyonight-moon",
-      --colorscheme = "tokyonight-night",
-      --colorscheme = "tokyonighti-strom",
-    },
-  },
+    {
+        'maxmx03/fluoromachine.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function ()
+         local fm = require 'fluoromachine'
+
+         fm.setup {
+            glow = true,
+            theme = 'fluoromachine',  -- Options: 'fluoromachine', 'retrowave', 'delta'
+            transparent = false,
+         }
+
+         vim.cmd.colorscheme 'fluoromachine'
+        end
+    }
 }
